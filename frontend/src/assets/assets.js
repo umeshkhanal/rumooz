03791a805a28bc954fdb2fb6d42486a77842fda9
Umesh Khanal <116002445+umeshkhanal@users.noreply.github.com>
2025-10-11 16:39:13 +0400

    Add files via upload

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,33 @@
+import dm from "./background/dm.jpg";
+import socialmedia from "./background/socialmedia.jpg";
+import graphic from "./background/graphic.jpg";
+import email from "./background/email.jpg";
+import photoshoot from "./background/photoshoot.jpg";
+import web from "./background/web.jpg";
+import videoedit from "./background/videoedit.jpg";
+import server from "./background/server.jpg";
+import PrestigeTower from "./buildings/PrestigeTower.jpg";
+import PrestigeTower1 from "./buildings/PrestigeTower1.jpg";
+import PrestigeTower2 from "./buildings/PrestigeTower2.jpg";
+import homebg from "./homebg.mp4";
+import rumooz from "./rumooz.mp4";
+import logo from "./logo.jpg";
+import logoName from "./logoName.jpg";
+
+export const assets = {
+  dm,
+  socialmedia,
+  graphic,
+  email,
+  web,
+  server,
+  photoshoot,
+  videoedit,
+  PrestigeTower,
+  PrestigeTower1,
+  PrestigeTower2,
+  logo,
+  logoName,
+  rumooz,
+  homebg
+};
